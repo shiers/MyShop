a1f9a35f711cd366288b42a23ca9965d16bc9458
sshiers <thekanack@dryad.ca>
2021-05-16 16:32:18 +1200

    Product Grouping adding

---
+++ MyShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Category Name")]
+        public string Category { get; set; }
+
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
